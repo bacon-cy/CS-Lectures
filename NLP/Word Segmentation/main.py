bf263606cbdb2dbd7b4621b8467cb1ab1dc275fe
bacon_cy <58839511+bacon-cy@users.noreply.github.com>
2024-02-27 00:25:35 +0800

    Add Comments and Reformat the code

--- NLP/Word Segmentation/main.py
+++ NLP/Word Segmentation/main.py
@@ -1,44 +1,42 @@
 # -*- coding: utf-8 -*-
-import time
-dict = {}
+
+# input the dictionary #
+dict = {}  # we use dictionary since it is made as a hashmap, the time complexity of searching key is O(1)
 dict_words_max_len = 0
-a = 0
-for line in open("160000_ch_dictionary.txt", "r", encoding="utf-8").readlines():
-    if a == 0:
-        line = line.encode('utf-8').decode('utf-8-sig')
-        a = line
-    length = len(line)
-    dict[line] = length
+dict_index = 0  # only use for removing BOM of UTF-8 in line 8 to 10
+for word in open("160000_ch_dictionary.txt", "r",encoding="utf-8").readlines():
+    # the first word contains the BOM of UTF-8, remove the BOM by this line
+    if dict_index == 0:
+        word = word.encode('utf-8').decode('utf-8-sig')
+        dict_index = -1
+    # add the word into the dictionary, the (key:value) pair is (word, length of the word)
+    length = len(word)
+    dict[word] = length
+    # record the max length of all words in the dictionary
     if dict_words_max_len < length:
         dict_words_max_len = length
+
+# BMM segmentation algorithm #
 input_string = input("請輸入句子或文章:\n ")
-time1 = time.time()
-input_string_len = len(input_string)
-output_list = []
-pointer = input_string_len # scan from the end
+input_string_len = len(input_string)  # the length of input string
+output_list = []  # the list records all word segments
+pointer = input_string_len  # the pointer points out the current scanning end
 while pointer > 0:
     for length in range(dict_words_max_len + 1, 0, -1):
         if pointer - length < 0:
-            continue
-        str = input_string[(pointer - length):pointer]
+            continue  # meaning that the testing-argument would be null or repeated, thus, skip it
+        str = input_string[(pointer - length):pointer]  # the current testing-segment
         exists = dict.get(str + '\n', -1)
-        if exists > 0:
-            output_list.append(str)
-            pointer = pointer - length
+        if exists > 0:  # if the testing-segment str exists, the variable exists must > 0, since the value in dict is 
+            # the length of words
+            output_list.append(str)  # append the existing segment to output list
+            pointer = pointer - length  # move the pointer over the current testing-segment
             break
         if length == 1:
-            output_list.append(input_string[pointer - 1])
+            output_list.append(input_string[pointer - 1])  # if there is none of word end with input_string[pointer - 1],
+            # let add the character to the output list anyway
             pointer -= 1
-output_list.reverse()
-c = 0
-print('[', end='')
-for s in output_list:
-    if c == 0:
-        c = 1
-    else:
-        print(', ', end='')
-    print('\'' + s + '\'', end='')
-print(']', end='')
+output_list.reverse()  # since we use BMM, reverse the output list
 
-time2 = time.time()
-# print(f"執行時間：{time2-time1:.4f}秒")
+# output the result #
+print(output_list)
